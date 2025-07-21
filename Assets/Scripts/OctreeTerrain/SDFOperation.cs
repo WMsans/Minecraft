78f0a05eba153879a15ebf4ab6671b664d8c71f4
WMsans <ggajy123@gmail.com>
2025-07-21 00:14:18 -0400

    Revert "delete sdf version of terrain"
    This reverts commit bd9ff64d242ae93b6d456e170d1fbeed4ce97d3d.

---
+++ Assets/Scripts/OctreeTerrain/SDFOperation.cs
@@ -0,0 +1,12 @@
+namespace OctreeTerrain
+{
+    public enum SDFOperation
+    {
+        Union,
+        Subtraction,
+        Intersection,
+        SmoothUnion,
+        SmoothSubtraction,
+        SmoothIntersection
+    }
+}
