89ba81c93b147def5260fbe55f15ff90f35c5511
WMsans <ggajy123@gmail.com>
2025-07-29 20:43:05 -0400

    Delete old files

---
+++ Assets/Scripts/Terrain Customization/Terrain Operation Layers/ApplyDefaultTextureTerrainLayer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Mathematics;
+using UnityEngine;
+
+public unsafe struct ApplyDefaultTextureTerrainLayer : ITerrainLayer
+{
+    [BurstCompile]
+    public static void Apply(ref TerrainLayer layer, int seed, float* density, byte* voxelTypes, int densityLength, int chunkSize, in float3 offset, float scale, void* entities)
+    {
+        if (!layer.enabled) return;
+
+        for (int i = 0; i < densityLength; i++)
+        {
+            voxelTypes[i] = 0;
+        }
+    }
+
+    public static TerrainLayer Create(params float[] properties)
+    {
+        return Create();
+    }
+
+    public static TerrainLayer Create()
+    {
+        var layer = new TerrainLayer
+        {
+            ApplyFunction = BurstCompiler.CompileFunctionPointer<TerrainLayer.ApplyDelegate>(Apply),
+            enabled = true,
+        };
+
+        return layer;
+    }
+    
+    /// <summary>
+    /// Defines the user-facing names for the layer's properties.
+    /// </summary>
+    public static string[] Fields() => Array.Empty<string>();
+}
